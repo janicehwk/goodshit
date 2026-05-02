81142b9dfbaf77601ce456fa1c5dd8b77c2f5517
janicehwk <hwkjanice@gmail.com>
2026-05-02 14:40:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+
+st.title("This is a title")
+st.header("This is a header")
+st.subheader("This is a subheader")
 st.write("hello, *world* :sunglasses:")
 
 st.write("ISOM5240")
